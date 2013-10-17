9ff9480858b7767cee55889bba2a78c82eaf2379
Michael Heap <m@michaelheap.com>
2013-10-17 20:44:05 +0100

    Can now add a card with a title and a description

--- src/commands/add-card.js
+++ src/commands/add-card.js
@@ -7,37 +7,54 @@ var __ = function(program, output, logger, config, trello, translator){
   .help("Add a card to a board")
   .options({
     "title": {
-      position: 0,
+      position: 1,
       help: "The card's title",
+      list: true,
+      required: true
+    },
+    "description": {
+      position: 2,
+      help: "The card's description",
       list: true
     },
     "board": {
       abbr: 'b',
       metavar: 'BOARD',
-      help: "The board name to add a card to"
+      help: "The board name to add a card to",
+      required: true
     },
     "list": {
       abbr: 'l',
       metavar: 'LIST',
-      help: "The list name to add a card to"
+      help: "The list name to add a card to",
+      required: true
     }
   })
-  .callback(function(board, list){
+  .callback(function(opts){
+    console.log(opts);
     logger.info("Adding card");
 
-    if (!program.title){
+    if (!opts.title){
       logger.warning("Please provide a card title")
       return;
     }
 
     // Grab our boards etc
-    var boardId = translator.getBoardIdByName(board);
-    var listId = translator.getListIdByBoardNameAndListName(board, list);
+    var boardId = translator.getBoardIdByName(opts.board);
+    var listId = translator.getListIdByBoardNameAndListName(opts.board, opts.list);
 
-    trello.post("/1/cards", {
-      "name": cardTitle,
+    // Build up arguments to send
+    var params = {
+      "name": opts.title,
       "idList": listId
-    }, function(err, data){
+    };
+
+    // Did we have any additional params?
+    if (opts.description){
+      params.desc = opts.description;
+    }
+
+    trello.post("/1/cards", params, function(err, data){
       if (err){ throw err; }
       logger.info("Card created");
     });
