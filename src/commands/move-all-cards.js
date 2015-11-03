3f41a829234b28842ce8c404f12c39dac8feb745
Simon Kittle <simon@kittle.co.uk>
2015-11-03 09:29:58 +0000

    Implemented Trello's move-all-cards command

---
+++ src/commands/move-all-cards.js
@@ -0,0 +1,72 @@
+var __ = function (program, output, logger, config, trello, translator) {
+
+  program
+  .command("move-all-cards")
+  .help("Move all cards from one list to another")
+  .options({
+    "sourceBoard": {
+      abbr: 'b',
+      metavar: 'BOARD',
+      help: "The board containing the list to move",
+      required: true
+    },
+    "sourceList": {
+      abbr: 'l',
+      metavar: 'LIST',
+      help: "The list containing the cards to move",
+      required: true
+    },
+    "destinationBoard": {
+      abbr: 'c',
+      metavar: 'BOARD',
+      help: "The destination board",
+      required: true
+    },
+    "destinationList": {
+      abbr: 'd',
+      metavar: 'LIST',
+      help: "The destination list",
+      required: true
+    },
+    "verbose": {
+      abbr: 'v',
+      help: "Turn on increased error reporting",
+      required: false,
+      flag: true
+    }
+  })
+  .callback(function (opts) {
+    logger.info("Moving cards");
+
+    // Grab our boards etc
+    var sourceListId, destinationBoardId, destinationListId;
+
+    try {
+      sourceListId = translator.getListIdByBoardNameAndListName(opts.sourceBoard, opts.sourceList);
+      destinationBoardId = translator.getBoardIdByName(opts.destinationBoard);
+      destinationListId = translator.getListIdByBoardNameAndListName(opts.destinationBoard, opts.destinationList);
+    } catch (err) {
+      logger.error(err.message + " (try refreshing the cache with 'refresh' command, or check spelling).");
+      if (!opts.verbose) {
+        process.exit(1);
+      } else {
+        throw err;
+      }
+    }
+
+    // Build up arguments to send
+    var params = {
+      "idBoard": destinationBoardId,
+      "idList": destinationListId
+    };
+
+    trello.post("/1/lists/" + sourceListId + "/moveAllCards", params, function (err, data) {
+      if (err) {
+        throw err;
+      }
+      logger.info(data.length + (data.length == 1 ? " card moved" : " cards moved"));
+    });
+
+  });
+}
+module.exports = __;
