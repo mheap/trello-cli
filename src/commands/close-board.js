c073cf995008de73b59c500aa0858621e1ac2991
Simon Kittle <simon@kittle.co.uk>
2015-11-10 12:36:14 +0000

    Close board command implemented

---
+++ src/commands/close-board.js
@@ -0,0 +1,77 @@
+"use strict";
+
+var async = require("async");
+
+var __ = function (program, output, logger, config, trello, translator, trelloApiCommands) {
+
+    var trelloApiCommand = {};
+
+    trelloApiCommand.makeTrelloApiCall = function (options, onComplete) {
+
+        var boardsToDelete = translator.getBoardIdsByName(options.boardNameMatch, function(boardNameToTest, match) { return boardNameToTest.indexOf(match) != -1; });
+
+        output.normal("Boards which will be closed:");
+        boardsToDelete.forEach(function (boardToDelete) {
+            output.normal("    " + boardToDelete.name);
+        });
+
+        // prompt for continue
+        output.normal("Do you want to continue with closing the boards? Type 'yes' to continue");
+
+        process.stdin.once('data', function (userInput) {
+            if (userInput.toString().indexOf('yes') == 0) {
+                logger.info("Closing boards...");
+
+                async.each(
+                    boardsToDelete,
+                    function (boardToDelete, callback) {
+                        trello.put("/1/boards/" + boardToDelete.id + "/closed", { value: true }, function (err, data) {
+                            if (err) {
+                                throw err;
+                            }
+                            callback();
+                        });
+                    },
+                    function (err) {
+                        logger.info("Boards closed");
+                        process.stdin.unref();
+                    }
+                );
+
+            } else {
+                logger.info("Exitting");
+                process.stdin.unref();
+            }
+
+        });
+
+    };
+
+    trelloApiCommand.nomnomProgramCall = function () {
+        program
+            .command("close-board")
+            .help("Adds a new board with the specified name")
+            .options({
+                "boardNameMatch": {
+                    abbr: 'b',
+                    metavar: 'BOARD',
+                    help: "The text to search for in the board name; all boards with the specified text in the name will be closed.",
+                    required: true
+                },
+                "verbose": {
+                    abbr: 'v',
+                    help: "Turn on increased error reporting",
+                    required: false,
+                    flag: true
+                }
+                })
+            .callback(function (options) {
+                trelloApiCommand.makeTrelloApiCall(options);
+            });
+
+    };
+
+    return trelloApiCommand;
+}
+
+module.exports = __;
