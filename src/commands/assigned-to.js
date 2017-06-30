8ee71ae1d70aace005c776a0997777ead883526d
Jonathan de jong <jonathandejong02@gmail.com>
2017-06-30 12:46:04 +0200

    Various small formatting + refactoring "assigned-to-me" to "assigned-to"
    This now parses a command argument as a username, id, "me", or any other member identifier, no error handling yet, but can now find assigned cards of other users

--- src/commands/assigned-to.js
+++ src/commands/assigned-to.js
@@ -2,11 +2,20 @@ fs = require("fs");
 
 var __ = function(program, output, logger, config, trello, translator) {
   program
-    .command("assigned-to-me")
-    .help("Show cards that are currently assigned to you")
-    .callback(function() {
-      logger.info("Showing assigned cards");
-      trello.get("/1/members/me/cards", function(err, data) {
+    .command("assigned-to")
+    .help("Show cards that are currently assigned to any member specified")
+    .options({
+        id: {
+          position: 1,
+          help: "Specifies user to show assigned cards for. (id/username/\"me\")",
+          list: false,
+          required: true
+        }
+      })
+    .callback(function(options) {
+      if (!options.id) return
+      logger.info(`Showing assigned cards to ${options.id}`);
+      trello.get(`/1/members/${options.id}/cards`, function(err, data) {
         if (err) {
           throw err;
         }
@@ -20,7 +29,7 @@ var __ = function(program, output, logger, config, trello, translator) {
         }
 
         for (var j in cards) {
-          output.normal(translator.getBoard(j).underline);
+          output.normal("\n"+translator.getBoard(j).underline);
           for (var k in cards[j]) {
             output.normal("* " + cards[j][k]);
           }
