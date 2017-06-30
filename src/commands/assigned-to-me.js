efe9754073bc7c7d89044d8992a1e78a04859f2f
Jonathan de jong <jonathandejong02@gmail.com>
2017-06-30 13:04:09 +0200

    fix for 4ded5e5650e70ade785797c49b965e833833c94d forgot to change command name

--- src/commands/assigned-to-me.js
+++ src/commands/assigned-to-me.js
@@ -1,19 +1,19 @@
 fs = require("fs");
 
-var __ = function(program, output, logger, config, trello, translator) {
+var __ = function (program, output, logger, config, trello, translator) {
   program
-    .command("assigned-to")
+    .command("assigned-to-me")
     .help("Show cards that are currently assigned to any member specified")
     .options({
-        id: {
-          position: 1,
-          help: "Specifies user to show assigned cards for. (id/username/\"me\")"
-        }
-      })
-    .callback(function(options) {
+      id: {
+        position: 1,
+        help: "Specifies user to show assigned cards for. (id/username/\"me\")"
+      }
+    })
+    .callback(function (options) {
       id = options.id || "me"
       logger.info(`Showing assigned cards to ${id}`);
-      trello.get(`/1/members/${id}/cards`, function(err, data) {
+      trello.get(`/1/members/${id}/cards`, function (err, data) {
         if (err) {
           console.error(err);
           return
@@ -28,7 +28,7 @@ var __ = function(program, output, logger, config, trello, translator) {
         }
 
         for (var j in cards) {
-          output.normal("\n"+translator.getBoard(j).underline);
+          output.normal("\n" + translator.getBoard(j).underline);
           for (var k in cards[j]) {
             output.normal("* " + cards[j][k]);
           }
