4ff0902edc598a8aebad3871d68df0dda1388767
Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2017-07-01 09:21:19 -0700

    Updates to assigned-to-me command (#44)

--- src/commands/assigned-to-me.js
+++ src/commands/assigned-to-me.js
@@ -1,31 +1,56 @@
 fs = require("fs");
 
 var __ = function(program, output, logger, config, trello, translator) {
-  program
-    .command("assigned-to-me")
-    .help("Show cards that are currently assigned to you")
-    .callback(function() {
-      logger.info("Showing assigned cards");
-      trello.get("/1/members/me/cards", function(err, data) {
-        if (err) {
-          throw err;
-        }
+  var trelloApiCommand = {};
 
-        // Order the issues by board
-        var cards = {};
-        for (var i in data) {
-          var item = data[i];
-          cards[item.idBoard] = cards[item.idBoard] || [];
-          cards[item.idBoard].push(item.name.replace(/\n/g, ""));
-        }
+  trelloApiCommand.makeTrelloApiCall = function(options, onComplete) {
+    user = options.user || "me";
+    logger.info("Showing assigned cards to " + user);
+    trello.get("/1/members/" + user + "/cards", function(err, data) {
+      if (err) {
+        logger.error(
+          "Error while retrieving assigned cards for " + user + ":",
+          err.statusMessage ? err.statusMessage : data
+        );
+        return;
+      }
 
-        for (var j in cards) {
-          output.normal(translator.getBoard(j).underline);
-          for (var k in cards[j]) {
-            output.normal("* " + cards[j][k]);
-          }
+      // Order the issues by board
+      var cards = {};
+      for (var i in data) {
+        var item = data[i];
+        cards[item.idBoard] = cards[item.idBoard] || [];
+        cards[item.idBoard].push(item.name.replace(/\n/g, ""));
+      }
+
+      for (var j in cards) {
+        output.underline(translator.getBoard(j));
+        for (var k in cards[j]) {
+          output.normal("* " + cards[j][k]);
         }
-      });
+      }
     });
+  };
+
+  trelloApiCommand.nomnomProgramCall = function() {
+    program
+      .command("assigned-to-me")
+      .help(
+        "Show cards that are currently assigned to yourself, or any member specified"
+      )
+      .options({
+        user: {
+          position: 1,
+          help: "Specifies user to show assigned cards for. (id/username)",
+          required: false
+        }
+      })
+      .callback(function(options) {
+        trelloApiCommand.makeTrelloApiCall(options);
+      });
+  };
+
+  return trelloApiCommand;
 };
+
 module.exports = __;
